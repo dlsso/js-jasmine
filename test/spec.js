00877c6cc2e37d04db0b4a1671e0fb449b5d9c13
John Hamel <supertonesfan@hotmail.com>
2014-09-16 16:52:10 -0500

    Saved all changes

--- test/spec.js
+++ test/spec.js
@@ -1,10 +1,10 @@
 describe('Pluralize', function() {
   it('should return cat if we pass 1', function() {
-    expect( pluralize("cat", 1)).toBe("cat");
+    expect( pluralize("cat", 1) ).toBe("cat");
   });
 
   it('should return dogs if we pass 2', function() {
-    expect( pluralize("dog", 2)).toBe("dogs");
+    expect( pluralize("dog", 2) ).toBe("dogs");
   });
 
 });
