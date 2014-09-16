dfc00f9e78fbe9d04903fe8a0f0073fb6ccd5a6c
John Hamel <supertonesfan@hotmail.com>
2014-09-16 16:49:26 -0500

    Wrote tests

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,10 @@
-describe('module', function() {
-  it('should do something', function() {
-    expect(true).toEqual(false);
+describe('Pluralize', function() {
+  it('should return cat if we pass 1', function() {
+    expect( pluralize("cat", 1)).toBe("cat");
   });
+
+  it('should return dogs if we pass 2', function() {
+    expect( pluralize("dog", 2)).toBe("dogs");
+  });
+
 });
